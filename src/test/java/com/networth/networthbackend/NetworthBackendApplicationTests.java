c5654b98a7805738aab1eb22eb38fd875d56d23d
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-11-03 23:12:30 -0500

    Initial commit

---
+++ src/test/java/com/networth/networthbackend/NetworthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networth.networthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
