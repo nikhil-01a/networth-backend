bc3ab22caffdb8d50c27ccaba4665f5de70fe784
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-11-11 23:32:21 -0500

    added category repository

--- src/main/java/com/networth/networthbackend/db/CategoryRepository.java
+++ src/main/java/com/networth/networthbackend/db/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.networth.networthbackend.db;
 
+import com.networth.networthbackend.data.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
